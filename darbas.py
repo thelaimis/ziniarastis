665c6120aac9f853e24690600feb841f1c777986
Laimis13222 <laimonas.kirsinas1@gmail.com>
2020-07-12 14:21:30 +0300

    papildymas

--- darbas.py
+++ darbas.py
@@ -1,11 +1,32 @@
 #funkcijos kurimas/kvietimas
 def labas():
-    print("labas")
+    print("printinu funkcijos vidu")
+
 labas()
 
 #duodam parametrus funkcijai
-
 def duodam_parametra(x,y):
-    return x+y
-
+     return x+y
 print(duodam_parametra(5,5))
+
+
+#tuple kurimas,kvietimas
+musu_tuple=("kazkas","kazkas2")  # ju pakeisti nebegalesim. jie "amzini"
+print(musu_tuple[0])
+
+# dictonary kurimas
+
+musu_dict={
+    "raktas_i":"atsakymas",
+    "raktas_i2":"atsakymas2"
+}
+print(musu_dict["raktas_i"]) #printas atsakymas
+
+#listo kurimas,panaudojimas
+musu_listas=["vienas","du","trys","keturi"]
+print(musu_listas[0])
+
+
+
+
+
